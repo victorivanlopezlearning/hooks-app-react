f18970c046d1a01e4423fb59603b52143e357bc7
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-08-30 17:21:22 -0600

    Used UserContext

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,8 +1,16 @@
+import { useContext } from "react"
+import { UserContext } from "./context/UserContext"
 
 export const LoginPage = () => {
+
+  const { user } = useContext(UserContext);
+
+  const { name } = user;
+
   return (
     <>
       <h2>Login</h2>
+      <h3>Bienvenido, <span className="fw-bold">{name}</span></h3>
     </>
   )
 }
--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,10 +1,15 @@
 import { UserContext } from './UserContext';
 
+const user = {
+  id: 123,
+  name: 'Víctor Iván López',
+  email: 'vlopez@vilgweb.com'
+}
 
 export const UserProvider = ({ children }) => {
   return (
     <UserContext.Provider
-      value={{}}
+      value={{user}}
     >
       {children}
     </UserContext.Provider>
