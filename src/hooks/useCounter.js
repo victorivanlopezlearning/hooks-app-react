8fc362addcb36cdc5e6db60b058fda05b6af7714
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-08-31 13:19:51 -0600

    Updated functions: onAdd and onSubtract

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -5,12 +5,12 @@ export const useCounter = (initialValue = 0) => {
   const [counter, setCounter] = useState(initialValue);
 
   const onAdd = (step = 1) => {
-    setCounter(counter + step);
+    setCounter((counter) => counter + step);
   };
 
   const onSubtract = (step = 1) => {
     if (counter === 0) return;
-    setCounter(counter - step);
+    setCounter((counter) => counter - step);
   };
 
   const onReset = () => {
